ca457cf3313014ccc22d588eeee9d26365aa3e9d
Jason Eriksen <730281+strandline@users.noreply.github.com>
2023-01-24 14:58:27 -0600

    Updating to get to current version

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="C:\Users\jason\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers
 git pull
 call webui.bat

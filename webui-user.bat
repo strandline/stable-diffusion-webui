b39fa9404ace457104c348f32e31df136f78ad73
Jason Eriksen <730281+strandline@users.noreply.github.com>
2023-03-29 12:13:55 -0500

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="C:\Users\jason\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --opt-split-attention --opt-channelslast --api
+set COMMANDLINE_ARGS= --xformers --opt-split-attention --opt-channelslast --api --no-half-vae
 git pull
 call webui.bat

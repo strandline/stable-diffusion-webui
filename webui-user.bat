e61b274ed5bc28e155580711c10010fcad49491a
Jason Eriksen <730281+strandline@users.noreply.github.com>
2023-11-17 08:35:41 -0600

    Update webui-user.bat
    Optimized startup files

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\jason\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=venv
+set COMMANDLINE_ARGS=--xformers --xformers-flash-attention --opt-split-attention --opt-sub-quad-attention --precision autocast --opt-channelslast
 
 call webui.bat

3499f33fde22108d8b6c475d62b77ba741645a42
Jason Eriksen <730281+strandline@users.noreply.github.com>
2024-01-06 08:53:45 -0600

    Update webui-user.bat with new COMMANDLINE_ARGS

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=C:\Program Files\Git\bin\git.exe
 set VENV_DIR=venv
-set COMMANDLINE_ARGS=--xformers --opt-channelslast --update-check --autolaunch
+set COMMANDLINE_ARGS=--xformers --no-half-vae --opt-channelslast --update-check --autolaunch
 
 rem https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Optimizations
 

6aa056ef3c06b0dd6347a6e721213ae08f40544f
Jason Eriksen <730281+strandline@users.noreply.github.com>
2024-01-06 08:58:49 -0600

    Update PYTHON path in webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON=C:\Python3_10_11\python.exe
+set PYTHON=C:\Python_3_10_11\python.exe
 set GIT=C:\Program Files\Git\bin\git.exe
 set VENV_DIR=venv
 set COMMANDLINE_ARGS=--xformers --no-half-vae --opt-channelslast --update-check --autolaunch
